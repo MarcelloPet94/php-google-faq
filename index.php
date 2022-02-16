dca9fd5785e8ef01891b5d0c6bc15ebc78b911d5
marcello <marcellopettenuzzo2@gmail.com>
2022-02-16 17:52:52 +0100

    ultimo

--- index.php
+++ index.php
@@ -45,7 +45,7 @@ $gfaq = [
     ],   
     [
         "question" => "Quando faccio clic sui risultati della Ricerca Google, le mie chiavi di ricerca vengono inviate ai siti web?",
-        "answer" => "In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, il tuo browser web potrebbe reindirizzare alla pagina web di destinazione anche l'indirizzo Internet, o URL, della pagina dei risultati di ricerca sotto forma di URL referrer. Talvolta, l'URL della pagina dei risultati di ricerca potrebbe contenere la query di ricerca che hai inserito. Se utilizzi la ricerca SSL (la funzione di ricerca criptata di Google), nella maggior parte dei casi i termini di ricerca non vengono inviati come parte dell'URL negli URL referrer. Questo comportamento può fare eccezione, ad esempio se utilizzi alcuni browser meno diffusi. Ulteriori informazioni sulla ricerca SSL sono disponibili qui. Le query di ricerca o le informazioni contenute nell'URL referrer potrebbero essere disponibili mediante Google Analytics o un'API (Application Programming Interface). Inoltre, gli inserzionisti potrebbero ricevere informazioni relative all' esatte parole chiave che hanno determinato il clic su un annuncio."
+        "answer" => "In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, il tuo browser web potrebbe reindirizzare alla pagina web di destinazione anche l'indirizzo Internet, o URL, della pagina dei risultati di ricerca sotto forma di URL referrer. Talvolta, l'URL della pagina dei risultati di ricerca potrebbe contenere la query di ricerca che hai inserito. Se utilizzi la ricerca SSL (la funzione di ricerca criptata di Google), nella maggior parte dei casi i termini di ricerca non vengono inviati come parte dell'URL negli URL referrer. Questo comportamento può fare eccezione, ad esempio se utilizzi alcuni browser meno diffusi. Ulteriori informazioni sulla ricerca SSL sono disponibili qui. Le query di ricerca o le informazioni contenute nell'URL referrer potrebbero essere disponibili mediante Google Analytics o un'API (Application Programming Interface). Inoltre, gli inserzionisti potrebbero ricevere informazioni relative all' esatte parole chiave che hanno determinato il clic su un annuncio.",
     ],          
 ];
 
@@ -65,6 +65,7 @@ $gfaq = [
             margin: 0;
             padding:0;
             box-sizing: border-box;
+            line-height: 1.5rem;
         }
         nav
         {
@@ -88,7 +89,6 @@ $gfaq = [
         .menu_tabs a
         {
             display:inline-block;
-            line-height: 2em;
             padding: 8px;
         }
         .menu_tabs a:hover
@@ -105,9 +105,14 @@ $gfaq = [
         }
         ul li
         {
-            margin-left: 20px;
+            
+            margin: 16px 0 16px 20px;
             list-style-type: decimal;
             list-style-position: outside;
+        }
+        ul li.bullet_deeper
+        {
+            list-style-type: lower-alpha;
         }
 
     </style>
